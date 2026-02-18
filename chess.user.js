6207c0e634b896c61e5d1bf899b9d27e414f6297
Người Bí Ẩn <alexanderacefischer@gmail.com>
2026-02-18 21:36:44 +0700

    Update chess.user.js

--- chess.user.js
+++ chess.user.js
@@ -1,17 +1,18 @@
-// ==UserScript==
-// @name         Hiếu Chess.com Assist V2.5
+ // ==UserScript==
+// @name         nope
 // @namespace    https://example.com
-// @version      2.5
-// @description  Chess.com Assist
+// @version      2.2
+// @description  Multi-engine (SF10/SF17/Local 17.1) + Auto-suggest + Auto-move + Collapsible UI
 // @author       Hiếu
 // @match        https://www.chess.com/*
+// @match        https://lichess.org/*
 // @require      https://cdnjs.cloudflare.com/ajax/libs/chess.js/0.10.3/chess.min.js
-// @require      https://raw.githubusercontent.com/Nguoibianhz/Chess-Assist/main/core.js
 // @grant        GM_addStyle
 // @grant        GM_xmlhttpRequest
 // @grant        unsafeWindow
-// @connect     raw.githubusercontent.com
-// @connect     127.0.0.1
-// @connect     localhost
+// @connect      127.0.0.1
+// @connect      localhost
+// @connect      stockfish.online
+// @connect      chess-api.com
 // @run-at       document-end
 // ==/UserScript==
